3b571e0426e40c7e2c580b0c4afb45e4896b0994
Michael Diers <md@sreid.de>
2021-11-15 14:35:25 +0100

    cors

--- frontend/frame/functions/app/middleware/base-middleware.js
+++ frontend/frame/functions/app/middleware/base-middleware.js
@@ -28,11 +28,12 @@ const initialize = (options = {}) => {
       },
     }),
   );
-  router.use(cors({
-    origin: [accountBaseUrl],
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+  router.use(cors());
+  // router.use(cors({
+  //  origin: [accountBaseUrl],
+  //  methods: ['GET', 'POST'],
+  //  allowedHeaders: ['Content-Type', 'Authorization'],
+  // }));
   router.use(compression());
   router.use(express.urlencoded({ extended: false }));
   router.use(express.json());
